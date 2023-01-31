eb7f920fc3581e73c40a7d35c135d497c1f78daa
arturo <arturofleitasc@gmail.com>
2023-01-31 12:53:33 -0300

    commit inicial

---
+++ WebApiV1/Controllers/SupesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiV1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Supes>>> Get()
+        {
+            var supers = new List<Supes>()
+            {
+                new Supes
+                {
+                    id = 1,
+                    name = "Batman",
+                    nombre = "Bruce",
+                    apellido = "Wayne",
+                    lugar = "Gotham City"
+                }
+            };
+
+            return Ok(supers);
+        }
+    }
+}
---
+++ WebApiV1/Supes.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace WebApiV1
+{
+    public class Supes
+    {
+        public int id { get; set; }
+
+        public string name { get; set; } = string.Empty;
+
+        public string nombre { get; set; } = string.Empty;
+
+        public string apellido { get; set; } = string.Empty;
+
+        public string lugar { get; set; } = string.Empty;
+    }
+}
